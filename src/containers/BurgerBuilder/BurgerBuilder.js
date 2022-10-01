d5c425dae8b7185dc35f30aa5f53105139390f57
daniel_eln <omoniyiopemipo6@gmail.com>
2022-10-01 16:09:07 +0100

    built the burger component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import React,{Component} from "react";
 
 import Auxiliary from "../../hoc/Auxiliary";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component{
     render() {
         return (
             <Auxiliary>
-                <div>Graphic rep of the burger</div>
+                <Burger />
                 <div>Build Controls</div>
             </Auxiliary>
         );
