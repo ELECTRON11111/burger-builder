bf11d23cd05ff980dd43a80ba760bbf540df51a9
daniel_eln <omoniyiopemipo6@gmail.com>
2022-10-02 22:29:32 +0100

    worked on buildControls and dynamically adding ingredinets to the burger - prepared logic for removal of ingredients and logic for disabled button

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,7 +4,7 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             /** props.ingredients[igKey] gets the value for the igKey e.g 2
              * [...Array(no from the above)] e.g [...Array(2)] - ["",""]
@@ -12,7 +12,15 @@ const burger = (props) => {
             return [...Array(props.ingredients[igKey])].map((_ , i) => {
                 return <BurgerIngredient key={igKey + i} type = {igKey} />
             })
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
+
 
     return (
         <div className={classes.Burger}>
