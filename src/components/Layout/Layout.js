083ae7faad8395ee77240932d585868947e3e57f
daniel_eln <omoniyiopemipo6@gmail.com>
2022-10-04 12:20:41 +0100

    Adding the toolBar component to the nav

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Auxiliary from "../../hoc/Auxiliary";
 import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = (props) => {
     return (
         <Auxiliary>
             {/* This div should contain the Toolbar, backdrop or Sidedrawer */}
-            <div>Toolbar, Sidedrawer, Backdrop</div>
+            {/* <div>Toolbar, Sidedrawer, Backdrop</div> */}
+            <Toolbar />
             <main className={classes.Content}>
                 {props.children}
             </main>
