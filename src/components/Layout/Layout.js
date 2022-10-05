7fc55aec284c83c20751de3539eefebe8494f2fb
daniel_eln <omoniyiopemipo6@gmail.com>
2022-10-05 18:11:49 +0100

    created and made the sideDrawer and Navigation items

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,13 +3,15 @@ import React from "react";
 import Auxiliary from "../../hoc/Auxiliary";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => {
     return (
         <Auxiliary>
             {/* This div should contain the Toolbar, backdrop or Sidedrawer */}
             {/* <div>Toolbar, Sidedrawer, Backdrop</div> */}
-            <Toolbar />
+            <Toolbar /> 
+            <SideDrawer />
             <main className={classes.Content}>
                 {props.children}
             </main>
