e65a00265709c68e7d4157c49671e21ba4d1ff4c
daniel_eln <omoniyiopemipo6@gmail.com>
2022-10-06 00:39:07 +0100

    using lifecycles hooks like shouldComponentUpdate and componentDidUpdate to check and prevent un-necessary re-rendering cycles so as to improve Performance of the application e.g with the Modal Component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,31 +3,40 @@ import React from "react";
 import Auxiliary from "../../../hoc/Auxiliary";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends React.Component {
+    componentDidUpdate() {
+        // This shows us how many times this is updated and is re-rendered. To plan App Optimization
+        console.log("[OrderSummary.js] componentDidUpdate");
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igkey => {
             return (
                 <li key={igkey}>
-                    <span style={{textTransform: "capitalize"}}>{igkey}</span>:{props.ingredients[igkey]}
+                    <span style={{textTransform: "capitalize"}}>{igkey}</span>:{this.props.ingredients[igkey]}
                 </li>
                 );
         });
 
-    // <li>Salad: 1</li>
-    return (
-        <Auxiliary>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following Ingredients</p>
+        // <li>Salad: 1</li>
 
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType = "Danger" clicked={props.cancelled}>CANCEL</Button>
-            <Button btnType = "Success" clicked={props.continued}>CONTINUE</Button>
-        </Auxiliary>
-    )
+        return (
+            <Auxiliary>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following Ingredients</p>
+    
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType = "Danger" clicked={this.props.cancelled}>CANCEL</Button>
+                <Button btnType = "Success" clicked={this.props.continued}>CONTINUE</Button>
+            </Auxiliary>
+        );
+    }
+    
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,19 +5,32 @@ import classes from "./Modal.module.css";
 import Auxiliary from "../../../hoc/Auxiliary";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => (
-    <Auxiliary>
-        <Backdrop show = {props.show} clicked={props.modalClosed}/>
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show? "translateY(0)": "translateY(-100vh)",
-                opacity: props.show? "1": "0"
-            }}
-        >
-            {props.children}
-        </div>
-    </Auxiliary>
-);
+class Modal extends React.Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    componentDidUpdate() {
+        // This shows us how many times this is updated and is re-rendered. To plan App Optimization
+        console.log("[modal.js] componentDidUpdate");
+    }
+
+    render() {
+        return (
+            <Auxiliary>
+                <Backdrop show = {this.props.show} clicked={this.props.modalClosed}/>
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show? "translateY(0)": "translateY(-100vh)",
+                        opacity: this.props.show? "1": "0"
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+        )
+    }
+}  
+
+export default Modal;
